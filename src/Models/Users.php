f9d2923c49c449dd682a93b720e776ad535fa936
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-03-24 18:47:43 +0000

    Create a function that check if user exists

--- src/Models/Users.php
+++ src/Models/Users.php
@@ -17,5 +17,33 @@
         public function __construct($db){
             $this -> conn = $db;
         }
+
+        //Create User
+        public function register(){
+
+        }
+
+        //Check if user exists
+        public function check_user(){
+            //Database Query
+            $query = 'SELECT COUNT(*) AS no_user FROM users WHERE username = :username';
+
+            //Prepare statement: Used to prepate sql query without the data.
+            $stmt = $this->conn->prepare($query);
+
+            //Execute the query with the data
+            $stmt->execute(array("username" => $this->username));
+            $row = $stmt->fetch();
+
+            //Cast to integer to make sure
+            settype($row["no_user"], "integer");
+
+            //If clause to check if a user exists
+            if($row['no_user'] !== 0){
+                return false;
+            } else {
+                return true;
+            }    
+        }
     }
 ?>
