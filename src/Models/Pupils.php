1257be58ccd0b52a4920629daa607269bb39f21f
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-03-27 15:08:08 +0000

    Add setter for name

--- src/Models/Pupils.php
+++ src/Models/Pupils.php
@@ -11,11 +11,42 @@
         private $address;
         private $date_of_birth;
         private $class_name;
+        private $medicals;
 
-        
+
         //Construct function to use the database
         public function __construct($db){
             $this->conn = $db;
+        }
+
+        public function set_name($first_name, $middle_initial, $last_name) {
+            // Trim whitespace
+            $first_name = trim($first_name);
+            $middle_initial = trim($middle_initial);
+            $last_name = trim($last_name);
+        
+            // Check if first and last name are empty
+            if (empty($first_name)) {
+                throw new Exception("First name can't be empty!");
+            }
+            if (empty($last_name)) {
+                throw new Exception("Last name can't be empty!");
+            }
+        
+            // Ensure names contain only letters
+            if (!ctype_alpha($first_name) || !ctype_alpha($last_name)) {
+                throw new Exception("Names must contain only alphabetic characters.");
+            }
+        
+            // Middle initial is optional, but if provided, must be a single alphabetic character
+            if (!empty($middle_initial) && (!ctype_alpha($middle_initial) || strlen($middle_initial) > 1)) {
+                throw new Exception("Middle initial must be a single letter.");
+            }
+        
+            // Assign values
+            $this->first_name = $first_name;
+            $this->middle_initial = $middle_initial;
+            $this->last_name = $last_name;
         }
     }
 ?>
