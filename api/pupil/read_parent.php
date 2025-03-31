c35d8d4119e0511e6273d1e768fe05198959e29a
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-03-31 16:06:20 +0100

    Refine comment and add status codes to pupil/read_parent.php endpoint

--- api/pupil/read_parent.php
+++ api/pupil/read_parent.php
@@ -1,20 +1,30 @@
 <?php
-    // Headers
+    // Portions of this code are adapted from:
+    // Traversy, B. (2019) 'PHP REST API - MyBlog', GitHub. Available at: 
+    // https://github.com/bradtraversy/php_rest_myblog.
+
+    // Header details explained in users/register.php
+    // Adapted from Traversy, B. (2019) 'PHP REST API - MyBlog'
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
+    // No other header is required as this api will be accessed through get request.
+    // End of external code.
 
-    //Import required files.
+    // My custom code.
+    // Import required files.
     require_once __DIR__."/../../config/Database.php";
     require_once __DIR__."/../../src/Models/Pupils.php";
     require_once __DIR__."/../../src/Middleware/auth_middleware.php";
 
-    //Protect route
+    // Protect route
     $user = authorize_parent();
+    // End of my custom code.
 
-    //Instantiate database and connect it. 
-    //Taken from https://github.com/bradtraversy/php_rest_myblog.
+    // Adapted from Traversy, B. (2019) 'PHP REST API - MyBlog'
+    // Instantiate database and connect it. 
     $database = new Database();
     $db = $database->connect();
+    // End of external code.
 
     //Instantiate a new pupil
     $pupil = new Pupil($db);
@@ -27,18 +37,20 @@
             if($pupil->check_parent($user->username, $id)){
                 $data = $pupil->read_single($id);
             }else{
-                throw new Exception ("Unauthorized!");
+                throw new Exception ("Unauthorized!", 401);
             }
             
         }else{
             $data = $pupil->read_parent($user->username);
         }
 
+        http_response_code(200);
         echo json_encode(array(
             "pupils" => $data
         ));
         
     }catch(Exception $e){
+        http_response_code($e->getCode());
         echo json_encode(array(
             "message" => $e->getMessage() 
         ));
