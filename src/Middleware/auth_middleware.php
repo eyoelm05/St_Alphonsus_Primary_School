615357d67c9663de035c326bf3e1a46167e68187
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-03-31 11:41:51 +0100

    Refine comment and add status codes to auth controller and middleware

--- src/Middleware/auth_middleware.php
+++ src/Middleware/auth_middleware.php
@@ -1,12 +1,15 @@
 <?php
+    // The code in this file is entirely custom made by me.
+    
     require __DIR__."/../Controller/auth_controller.php";
 
     function authorize_parent(){
-        //Call authenticate function
+        // Call authenticate function
         $users = authenticate();
 
-        //Check if user type is parent $users->user_type because by default decoding a jwt token gives you an object
+        // Check if user type is parent $users->user_type because by default decoding a jwt token gives you an object
         if($users->user_type != "parent"){
+            http_response_code(401);
             echo json_encode(["message" => "Access Denied: Parents"]);
             exit();
         }
@@ -15,11 +18,12 @@
     }
 
     function authorize_employee(){
-        //Call authenticate function
+        // Call authenticate function
         $users = authenticate();
 
-        //Check if user type is employee
+        // Check if user type is employee
         if($users->user_type != "employee"){
+            http_response_code(401);
             echo json_encode(["message" => "Access Denied: Employee's only"]);
             exit();
         }
