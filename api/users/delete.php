316066b1b978540062caafb65f7f0fec90afc95a
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-04-04 18:10:50 +0100

    Change request type to delete

--- api/users/delete.php
+++ api/users/delete.php
@@ -9,7 +9,7 @@
     // Adapted from Traversy, B. (2019) 'PHP REST API - MyBlog'
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Methods: Delete');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
     // End of external code.
 
